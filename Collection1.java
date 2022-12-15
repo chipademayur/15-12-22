06050ccae019bc5556aaff8bfb4d383bbc39ef48
chipademayur <101926125+chipademayur@users.noreply.github.com>
2022-12-15 21:56:16 +0530

    Add files via upload

---
+++ Collection1.java
@@ -0,0 +1,19 @@
+package collection;
+import java.util.ArrayList;
+
+public class Collection1 {
+
+	public static void main(String[] args) {
+		ArrayList <String> ob=new ArrayList<>();
+		ob.add("Accenture");
+		ob.add("infosys");
+		ob.add("TCS");
+		ob.add("wipro");
+		ob.add("capgemini");
+		System.out.println("IT Companies are:"+ob.size());
+		System.out.println(ob);
+		
+
+	}
+
+}
